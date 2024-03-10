e4e03c155558815ebca23e1ce9d761831c1a5844
Diryfort <dorhungmi2@gmail.com>
2024-03-10 20:19:38 +0300

    icon and new stickers

--- app/src/main/java/com/maxim/shacamera/stickers/data/StickersRepository.kt
+++ app/src/main/java/com/maxim/shacamera/stickers/data/StickersRepository.kt
@@ -33,6 +33,22 @@ interface StickersRepository {
                 StickerUi.Base(R.drawable.loading),
                 StickerUi.Base(R.drawable.artem),
                 StickerUi.Base(R.drawable.dama_pick),
+                StickerUi.Base(R.drawable.ad),
+                StickerUi.Base(R.drawable.boks),
+                StickerUi.Base(R.drawable.money),
+                StickerUi.Base(R.drawable.push_ups),
+                StickerUi.Base(R.drawable.wait),
+                StickerUi.Base(R.drawable.what),
+                StickerUi.Base(R.drawable.how),
+                StickerUi.Base(R.drawable.as_you_want),
+                StickerUi.Base(R.drawable.bread),
+                StickerUi.Base(R.drawable.cases),
+                StickerUi.Base(R.drawable.cooler),
+                StickerUi.Base(R.drawable.free_nitro),
+                StickerUi.Base(R.drawable.journal),
+                StickerUi.Base(R.drawable.sleng),
+                StickerUi.Base(R.drawable.sleng_2),
+                StickerUi.Base(R.drawable.wi_fi),
             )
         }
     }
